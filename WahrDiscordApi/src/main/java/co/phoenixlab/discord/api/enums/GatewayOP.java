dfae02db6c8e0fc318714a26218b8accf75919be
Vincent Zhang <vzhang@purdue.edu>
2016-04-09 00:45:42 -0400

    Add gateway opcode enum

---
+++ WahrDiscordApi/src/main/java/co/phoenixlab/discord/api/enums/GatewayOP.java
@@ -0,0 +1,44 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Vincent Zhang/PhoenixLAB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package co.phoenixlab.discord.api.enums;
+
+import co.phoenixlab.discord.api.gsonadapters.GatewayOPTypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+
+@JsonAdapter(GatewayOPTypeAdapter.class)
+public enum GatewayOP {
+
+    DISPATCH,
+    HEARTBEAT,
+    IDENTIFY,
+    STATUS_UPDATE,
+    VOICE_STATE_UPDATE,
+    VOICE_SERVER_PING,
+    RESUME,
+    RECONNECT,
+    REQUEST_GUILD_MEMBERS,
+    INVALID_SESSION,
+    UNKNOWN;
+
+    public static GatewayOP fromInt(int i) {
+        if (i >= 0 && i <= 9) {
+            return values()[i];
+        }
+        return UNKNOWN;
+    }
+
+    public int toInt() {
+        return ordinal();
+    }
+
+}
