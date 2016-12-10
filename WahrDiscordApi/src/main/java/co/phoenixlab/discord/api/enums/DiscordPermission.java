f0a3a6b16b5c3e4c593965c791859997d4d5756b
Vincent Zhang <vzhang@purdue.edu>
2016-12-10 13:35:39 -0500

    Update permissions

--- WahrDiscordApi/src/main/java/co/phoenixlab/discord/api/enums/DiscordPermission.java
+++ WahrDiscordApi/src/main/java/co/phoenixlab/discord/api/enums/DiscordPermission.java
@@ -18,40 +18,46 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum DiscordPermission {
-    GEN_CREATE_INSTANT_INVITE(0),
-    GEN_KICK_MEMBERS(1),
-    GEN_BAN_MEMBERS(2),
-    GEN_MANAGE_ROLES(3),
-    GEN_MANAGE_PERMISSIONS(4),
-    GEN_MANAGE_CHANNELS(5),
-    GEN_MANAGE_SERVER(6),
-    CHAT_READ_MESSAGES(10),
-    CHAT_SEND_MESSAGES(11),
-    CHAT_SEND_TTS_MESSAGES(12),
-    CHAT_MANAGE_MESSAGES(13),
-    CHAT_EMBED_LINKS(14),
-    CHAT_ATTACH_FILES(15),
-    CHAT_READ_MESSAGE_HISTORY(16),
-    CHAT_MENTION_EVERYONE(17),
-    VOICE_CONNECT(20),
-    VOICE_SPEAK(21),
-    VOICE_MUTE_MEMBERS(22),
-    VOICE_DEAFEN_MEMBERS(23),
-    VOICE_MOVE_MEMBERS(24),
-    VOICE_USE_VOICE_ACTIVITY_DETECTION(25);
+    CREATE_INSTANT_INVITE(0x00000001),
+    KICK_MEMBERS(0x00000002),
+    BAN_MEMBERS(0x00000004),
+    ADMINISTRATOR(0x00000008),
+    MANAGE_CHANNELS(0x00000010),
+    MANAGE_GUILD(0x00000020),
+    ADD_REACTIONS(0x00000040),
+    READ_MESSAGES(0x00000400),
+    SEND_MESSAGE(0x00000800),
+    SEND_TTS_MESSAGE(0x00001000),
+    MANAGE_MESSAGES(0x00002000),
+    EMBED_LINKS(0x00004000),
+    ATTACH_FILES(0x00008000),
+    READ_MESSAGE_HISTORY(0x00010000),
+    MENTION_EVERYONE(0x00020000),
+    USE_EXTERNAL_EMOJIS(0x00040000),
+    CONNECT(0x00100000),
+    SPEAK(0x00200000),
+    MUTE_MEMBERS(0x00400000),
+    DEAFEN_MEMBERS(0x00800000),
+    MOVE_MEMBERS(0x01000000),
+    USE_VAD(0x02000000),
+    CHANGE_NICKNAME(0x04000000),
+    MANAGE_NICKNAMES(0x08000000),
+    MANAGE_ROLES(0x10000000),
+    MANAGE_WEBHOOKS(0x20000000),
+    MANAGE_EMOJIS(0x40000000);
 
-    private final int bitNum;
+    private final long mask;
 
-    DiscordPermission(int bitNum) {
-        this.bitNum = bitNum;
+    DiscordPermission(long mask) {
+        this.mask = mask;
     }
 
-    public int getBitNum() {
-        return bitNum;
+    public long getMask() {
+        return mask;
     }
 
     public long toLong() {
-        return 1L << bitNum;
+        return mask;
     }
 
     public boolean test(long l) {
