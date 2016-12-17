fb98d98cb961e74d301d531b6bf52cb4173a0b04
Vincent Zhang <vzhang@purdue.edu>
2016-12-17 14:07:27 -0500

    Add integration representations

---
+++ WahrDiscordApi/src/main/java/co/phoenixlab/discord/api/entities/guild/Integration.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Vincent Zhang/PhoenixLAB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package co.phoenixlab.discord.api.entities.guild;
+
+import co.phoenixlab.discord.api.entities.Entity;
+import co.phoenixlab.discord.api.entities.user.HumanUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Getter
+@NoArgsConstructor
+public class Integration implements Entity {
+
+    private long id;
+    private String name;
+    private String type;
+    private boolean enabled;
+    private boolean syncing;
+    private long roleId;
+    private int expireBehavior;
+    private int expireGracePeriod;
+    private HumanUser user;
+    private IntegrationAccount account;
+    private Instant syncedAt;
+
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    @Override
+    public boolean equals(Object o) {
+        return Entity.areEqual(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Entity.hash(this);
+    }
+}
